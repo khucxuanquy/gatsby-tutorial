8331cbcc0782ce8da16a9f5a0a8a77202b7ad247
Jonah Khuc <jonah@offspringdigital.com>
2022-10-08 14:35:04 +0700

    update env

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,6 @@
+require('dotenv').config({
+  path: '.env'
+})
 module.exports = {
   siteMetadata: {
     title: `my-project`,
@@ -9,8 +12,8 @@ module.exports = {
     {
       resolve: "gatsby-source-prismic",
       options: {
-        repositoryName: 'jonah-go-dev',
-        customTypesApiToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0eXBlIjoibWFjaGluZTJtYWNoaW5lIiwiZGJpZCI6ImpvbmFoLWdvLWRldi1jMmEzNDQ3YS04YzA0LTRhZjgtODliMS0zZGNiNTRjNjJmN2VfNCIsImRhdGUiOjE2NjUxNTYxMDAsImRvbWFpbiI6ImpvbmFoLWdvLWRldiIsImlhdCI6MTY2NTE1NjEwMH0.i-YCY61Y8TortuvJh-lrHzblXRruxvSR4JxA3ZYMWYE'
+        repositoryName: process.env.REPOSITORY_NAME,
+        customTypesApiToken: process.env.CUSTOM_TYPES_API_TOKEN,
       }
     }
   ]
